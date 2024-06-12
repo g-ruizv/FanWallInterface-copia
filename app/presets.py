4c71a4683f4bba3fd9696edfd3a6f0c86661a30a
eltrillos <strillos@miuandes.cl>
2024-06-12 16:36:15 -0400

    fixed run_preset and added api route for same_size preset GET method

--- app/presets.py
+++ app/presets.py
@@ -5,8 +5,9 @@ from flask_cors import cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from . import app, db, mqtt_client
 from .models import Preset
-from .functions import generate_config_matrix
+from .functions import generate_config_matrix, get_dimensions_from_preset
 from .mqtt import mqtt_client, send_mqtt_message
+from .socket import send_fan_speed
 
 preset_thread = None
 stop_event = threading.Event()
@@ -59,13 +60,23 @@ def get_preset(id):
     preset = Preset.query.get(id)
     return {'id': preset.id, 'name': preset.name, 'data': preset.data}
 
+@app.route('/api/v1/fanWall/presets/same_size/<x>/<y>', methods=['GET'])
+@cross_origin()
+def get_presets_of_size(x,y):
+    presets = Preset.query.all()
+    return {'presets': [
+        {'id':preset.id,'name':preset.name, 'data':preset.data} 
+        for preset in presets
+        if get_dimensions_from_preset(preset.data["frames"][0]["matrix"]) == (int(x), int(y))
+        ]}
+
+
 @app.route('/api/v1/fanWall/presets/<presetId>/configuration/<configId>', methods=['GET'])
 @cross_origin()
 def run_preset(presetId, configId):
     global preset_thread, stop_event
     preset = Preset.query.get(presetId)
     matrix = generate_config_matrix(configId)
-
     stop_event.clear()
 
     preset_thread = threading.Thread(target=run_preset, args=(preset,matrix))
@@ -82,17 +93,13 @@ def stop_preset(presetId, configId):
 
     return {'status': 'Preset stopped'}
     
-    
-
-
 def run_preset(preset,matrix):
     while not stop_event.is_set():
         for frame in preset.data['frames']:
-            print(frame['matrix'])
             for i in range(len(frame['matrix'])):
-                for j in range(len(frame['matrix'][i])):
-                    print(i, j, frame['matrix'][i][j], matrix[i][j])
+                for j in range(len(frame['matrix'][i])):                  
                     controllerId = matrix[i][j]
                     controllerSetSpeed = frame['matrix'][i][j]
                     send_mqtt_message('fanwall/wall/' + controllerId,controllerSetSpeed,mqtt_client)
+                    send_fan_speed(controllerId, controllerSetSpeed)
             sleep(frame['time']/1000)   
