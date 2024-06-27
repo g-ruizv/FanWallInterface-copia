4608ddb1289ecd4d46a1eba3913177145ca86466
eltrillos <strillos@miuandes.cl>
2024-06-27 17:11:45 -0400

    added JSON validation on backend

--- app/functions.py
+++ app/functions.py
@@ -23,3 +23,49 @@ def get_dimensions_from_preset(matrix):
     xSize = len(matrix[0])
     ySize = len(matrix)
     return (xSize, ySize)
+
+def preset_frame_element_count(matrix):
+    count = 0
+    for row in matrix:
+        count += len(row)
+    return count
+
+
+def is_preset_frame_rectangle(matrix):
+    xSize, ySize = get_dimensions_from_preset(matrix)
+    return preset_frame_element_count(matrix) == xSize * ySize
+
+def validate_json(json):
+    print('xdd')
+    if 'frames' not in json or not isinstance(json['frames'], list):
+        return False, 'frames must be a list'
+    
+    if len(json['frames']) == 0:
+        return False, 'frames must have at least one element'
+    
+    rows, cols = get_dimensions_from_preset(json['frames'][0]['matrix'])
+    print(rows, cols)
+
+    for frame in json['frames']:
+        print(frame)
+        if is_preset_frame_rectangle(frame['matrix']):
+            return False, 'all frames must be rectangles'
+
+        if 'matrix' not in frame:
+            return False, 'matrix must be a key in each frame'
+        
+        if not isinstance(frame['matrix'], list):
+            return False, 'matrix must be a list'
+        
+        if len(frame['matrix']) != rows:
+            return False, 'all matrices must have the same number of rows'
+        
+        if len(frame['matrix'][0]) != cols:
+            return False, 'all matrices must have the same number of columns'
+        
+        for row in frame['matrix']:
+            if not all(isinstance(x, int) for x in row):
+                return False, 'all elements in the matrix must be integers'
+        
+    
+    return True, None
