291b1fe9afac2deef25677ecf5be563e5474504c
eltrillos <strillos@miuandes.cl>
2024-07-09 13:49:37 -0400

    Procfile fix v2

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     #socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), allow_unsafe_werkzeug=True)
